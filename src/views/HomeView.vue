326509d31821379bad725193eb758aa735ac484a
xk <xk@qq.com>
2023-02-20 16:56:24 +0800

    s

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,7 @@
         <el-tab-pane label="十字星" name="four"></el-tab-pane>
         <el-tab-pane label="下影线" name="five"></el-tab-pane>
         <el-tab-pane label="周线十字星" name="six"></el-tab-pane>
+        <el-tab-pane label="底部大涨" name="seven"></el-tab-pane>
       </el-tabs>
     </div>
 
@@ -97,6 +98,21 @@
 
     </div>
 
+    <div v-if="activeName == 'seven'" style="margin-top: 50px; padding: 0px 100px">
+      <div style="display: flex; justify-content: space-around; align-items:flex-start">
+
+        <div v-for="(item, index) of dibu_data">
+          <div style="display: flex; justify-content: center; align-items: center; flex-direction: column" >
+            <div style="margin-bottom: 20px">{{ item.label }}</div>
+              <div v-for="(item, index) of item.value" :key="index" style="margin-top: 5px">
+              <a target="_blank" :href="'https://xueqiu.com/S/' + item.stock_area + item.stock_code">{{ item.stock_name }}</a>
+              </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
 
   </div>
 </template>
@@ -118,7 +134,8 @@ export default {
       huimaq_data: [],
       shizixing_data: [],
       xiayingxian_data: [],
-      zhouxian_shizixing_data: []
+      zhouxian_shizixing_data: [],
+      dibu_data: []
 
     }
   },
@@ -129,6 +146,7 @@ export default {
     this.getShiZiXing()
     this.getXiaYingXian()
     this.getZhouShiZiXing()
+    this.getDiBu()
   },
   methods: {
 
@@ -194,6 +212,17 @@ export default {
       axios.get('/api/huimaq')
       .then(function (response) {
         that.huimaq_data = response.data
+        
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    getDiBu(){
+      let that= this
+      axios.get('/api/dibu')
+      .then(function (response) {
+        that.dibu_data = response.data
         
       })
       .catch(function (error) {
